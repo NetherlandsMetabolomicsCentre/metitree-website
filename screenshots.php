8d153908f0d18217887944079aa218a1c4bacdb0
Michael van Vliet <info@michaelvanvliet.nl>
2012-07-06 12:33:14 +0200

    added key_sort on screenshots

--- screenshots.php
+++ screenshots.php
@@ -8,23 +8,34 @@
 				<?php
 				$dir = "./screenshots/";
 
+		    	$arrFiles = array();
+
 				if (is_dir($dir)) {
 				    if ($dh = opendir($dir)) {
+				    
 				        while (($file = readdir($dh)) !== false) {
 				        		if (filetype($dir . $file) == 'file'){
 				        		
 				        			$arrExtension	= explode('.', $file);
 				        			$arrFilename	= explode('_', $arrExtension[0]);
 				        			$imageNo		= $arrFilename[0];
-				        			$imageNote		= str_replace($imageNo, "<b>".$imageNo."</b>. ", join(' ', $arrFilename));
-				        			$image			= $dir . $file;
-				        			echo '<img style="border: thin solid #000;" width="550px" src="'.$image.'"><br />';
-						            echo '<i>'.$imageNote.'</i><br /><br />';
+
+				        			$tempFile = array();
+				        			$tempFile['note']	= str_replace($imageNo, "<b>".$imageNo."</b>. ", join(' ', $arrFilename));
+				        			$tempFile['image']	= $dir . $file;
+						            
+						            $arrFiles[(int) $imageNo] = $tempFile;
 						        }
 				        }
 			        	closedir($dh);
 			    	}
 				}
+				ksort($arrFiles);
+				foreach ($arrFiles as $f){
+        			echo '<img style="border: thin solid #000;" width="550px" src="'.$f['image'].'"><br />';
+		            echo '<i>'. $f['note'] .'</i><br /><br />';
+				}
+				
 				?>
 			</div>
 		</div>
