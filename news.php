954f7661b4a87ad44acf61f6e53c7bea90d5a39e
Michael van Vliet <info@michaelvanvliet.nl>
2012-10-04 10:58:53 +0200

    Add news Metabomeeting poster Justin vd Hooft

---
+++ news.php
@@ -0,0 +1,19 @@
+<?php require_once('header.php'); ?>
+<h1 property="dc:title" datatype="">News</h1>
+
+<div class="content clearfix">
+    <div class="field field-name-body field-type-text-with-summary field-label-hidden">
+        <div class="field-items">
+            <div class="field-item even" property="content:encoded">
+                <div style="float: right">October 4th, 2012</div>
+                <h1>MSN SPECTRAL TREE DATABASES: ANNOTATION AND IDENTIFICATION OF PLANT METABOLITES</h1>
+                <img style="border: thin solid #dcdcdc; float: right; margin: 2px 0 10px 10px;" src="./img/Metabomeeting2012_September10_JJJvdHooft.jpg">
+Annotation and identification of metabolites detected in untargeted liquid chromatography coupled to mass spectrometry (LC-MS) profiling techniques is still a major challenge that needs to be overcome in order to interpret the results obtained in a biological context. Protocols for generating MSn and LC-MSn data from metabolites detectable in plants, including Arabidopsis leaves and tomato fruit, have recently been developed, resulting in robust spectral trees [1,2]. Meanwhile, a software tool called MEF has been developed to process the MSn raw data and assign elemental formulas to the detected metabolite MSn signals [3]. Very recently, a web-based program called MetiTree, embedding the MEF tool and algorithms to visualize and compare spectral trees [4] has become available [5].<br />
+<a href="./resources/Metabomeeting2012_September10_JJJvdHooft.pdf" target="_blank">read more...</a><br /><br />
+<a href="http://www.linkedin.com/pub/justin-van-der-hooft/35/a93/9aa" target="_blank">Justin J.J. van der Hooft</a> (1,2,3), <a href="http://www.linkedin.com/in/miquelrojascherto" target="_blank">Miguel Rojas-Cherto</a> (3), <a href="http://www.linkedin.com/pub/thomas-hankemeier/23/bab/a51" target="_blank">Thomas Hankemeier</a> (3), <a href="http://www.linkedin.com/in/michaelvanvliet" target="_blank">Michael van Vliet</a> (3), <a href="http://www.linkedin.com/pub/theo-reijmers/42/811/260" target="_blank">Theo Reijmers</a> (3), <a href="http://www.linkedin.com/pub/robert-hall/2/792/88" target="_blank">Robert D. Hall</a> (1,2,3) and <a href="http://www.linkedin.com/pub/ric-de-vos/17/601/309" taget="_blank">Ric C.H. de Vos</a> (1,2,3)<br /><br />
+<i><b>1</b>. <a href="http://www.wageningenur.nl/nl/Expertises-Dienstverlening/Onderzoeksinstituten/Plant-Research-International.htm" target="_blank">Plant Research International, Wageningen-UR, Wageningen, The Netherlands</a>; <b>2</b>. <a href="http://www.cbsg.nl/" target="_blank">Centre for BioSystems Genomics, Wageningen, The Netherlands</a>; <b>3</b>. <a href="http://www.metabolomicscentre.nl/" target="_blank">Netherlands Metabolomics Centre, Leiden, The Netherlands</a></i>
+            </div>
+        </div>
+    </div>
+</div>
+<?php require_once('footer.php'); ?>
