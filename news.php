b6ba6fc2ca19a59d83d5a38196b99482841a97ab
Michael van Vliet <info@michaelvanvliet.nl>
2012-11-16 13:10:28 +0100

    Added news item: An automated Metabolite Identification Pipeline using Mass Spectral Trees

--- news.php
+++ news.php
@@ -5,12 +5,47 @@
     <div class="field field-name-body field-type-text-with-summary field-label-hidden">
         <div class="field-items">
             <div class="field-item even" property="content:encoded">
-                <div style="float: right">October 4th, 2012</div>
+                <div style="margin-left:20px; float: right">November 16th, 2012</div>
+                <h1>An automated Metabolite Identification Pipeline using Mass Spectral Trees</h1>
+                <a href="./resources/Montreux2012_MI.pdf" target="_blank"><img style="border: thin solid #dcdcdc; float: right; margin: 2px 0 10px 10px;" src="./img/msn_montreux_2012.jpg"></a>
+Identifying metabolites has been reported as one of the major bottlenecks in metabolomics. In part, this is due to the absence of
+good computational tools to automate metabolite identification. To address this issue, we have developed mathematical tools to
+process and compare multi-stage mass spectrometry data (MSn), in order to extract as much as possible information from the
+fragmentation trees. In addition, candidate structures are generated computationally and filters are used to reject improbable
+chemical structures. This poster presents the integrated use of these tools in a pipeline fashion to identify metabolites in human
+urine.<br />
+<a href="./resources/Montreux2012_MI.pdf" target="_blank">read more...</a><br /><br />
+<a href="http://www.linkedin.com/pub/theo-reijmers/42/811/260" target="_blank">Theo Reijmers</a> (1),
+<a href="http://nl.linkedin.com/in/juliopeironcely" target="_blank">Julio Peironcely</a> (1,2),
+<a href="http://www.linkedin.com/in/miquelrojascherto" target="_blank">Miguel Rojas-Cherto</a> (1),
+<a href="http://nl.linkedin.com/pub/piotr-kasper/15/b95/256" target="_blank">Piotr Kasper</a> (1),
+<a href="nl.linkedin.com/pub/leon-coulier/6/8a2/648" target="_blank">Leon Coulier</a> (2),
+Albert Tas (2),
+<a href="nl.linkedin.com/pub/rob-vreeken/2/631/41b" target="_blank">Rob Vreeken</a> (1) and
+<a href="http://www.linkedin.com/pub/thomas-hankemeier/23/bab/a51" target="_blank">Thomas Hankemeier</a> (1)
+<br /><br />
+<i><b>1</b>. <a href="http://analyticalbiosciences.leidenuniv.nl/" taget="_blank">Analytical Biosciences, Leiden University</a>, </i>
+<i><b>2</b>. <a href="http://www.tno.nl/" taget="_blank">TNO Quality of Life, Zeist</a></i>
+            </div>
+            <div style="margin: 60px 0;"><hr></div>
+            <div class="field-item even" property="content:encoded">
+                <div style="margin-left:20px; float: right">October 4th, 2012</div>
                 <h1>MSn spectral tree databases: annotation and identification of plant metabolites</h1>
                 <a href="./resources/Metabomeeting2012_September10_JJJvdHooft.pdf" target="_blank"><img style="border: thin solid #dcdcdc; float: right; margin: 2px 0 10px 10px;" src="./img/Metabomeeting2012_September10_JJJvdHooft.jpg"></a>
-Annotation and identification of metabolites detected in untargeted liquid chromatography coupled to mass spectrometry (LC-MS) profiling techniques is still a major challenge that needs to be overcome in order to interpret the results obtained in a biological context. Protocols for generating MSn and LC-MSn data from metabolites detectable in plants, including Arabidopsis leaves and tomato fruit, have recently been developed, resulting in robust spectral trees [1,2]. Meanwhile, a software tool called MEF has been developed to process the MSn raw data and assign elemental formulas to the detected metabolite MSn signals [3]. Very recently, a web-based program called MetiTree, embedding the MEF tool and algorithms to visualize and compare spectral trees [4] has become available [5].<br />
+Annotation and identification of metabolites detected in untargeted liquid chromatography coupled to mass spectrometry (LC-MS) profiling
+techniques is still a major challenge that needs to be overcome in order to interpret the results obtained in a biological context. Protocols for
+generating MSn and LC-MSn data from metabolites detectable in plants, including Arabidopsis leaves and tomato fruit, have recently been
+developed, resulting in robust spectral trees [1,2]. Meanwhile, a software tool called MEF has been developed to process the MSn raw data and
+assign elemental formulas to the detected metabolite MSn signals [3]. Very recently, a web-based program called MetiTree, embedding the MEF
+tool and algorithms to visualize and compare spectral trees [4] has become available [5].<br />
 <a href="./resources/Metabomeeting2012_September10_JJJvdHooft.pdf" target="_blank">read more...</a><br /><br />
-<a href="http://www.linkedin.com/pub/justin-van-der-hooft/35/a93/9aa" target="_blank">Justin J.J. van der Hooft</a> (1,2,3), <a href="http://www.linkedin.com/in/miquelrojascherto" target="_blank">Miguel Rojas-Cherto</a> (3), <a href="http://www.linkedin.com/pub/thomas-hankemeier/23/bab/a51" target="_blank">Thomas Hankemeier</a> (3), <a href="http://www.linkedin.com/in/michaelvanvliet" target="_blank">Michael van Vliet</a> (3), <a href="http://www.linkedin.com/pub/theo-reijmers/42/811/260" target="_blank">Theo Reijmers</a> (3), <a href="http://www.linkedin.com/pub/robert-hall/2/792/88" target="_blank">Robert D. Hall</a> (1,2,3) and <a href="http://www.linkedin.com/pub/ric-de-vos/17/601/309" taget="_blank">Ric C.H. de Vos</a> (1,2,3)<br /><br />
+<a href="http://www.linkedin.com/pub/justin-van-der-hooft/35/a93/9aa" target="_blank">Justin J.J. van der Hooft</a> (1,2,3),
+<a href="http://www.linkedin.com/in/miquelrojascherto" target="_blank">Miguel Rojas-Cherto</a> (3),
+<a href="http://www.linkedin.com/pub/thomas-hankemeier/23/bab/a51" target="_blank">Thomas Hankemeier</a> (3),
+<a href="http://www.linkedin.com/in/michaelvanvliet" target="_blank">Michael van Vliet</a> (3),
+<a href="http://www.linkedin.com/pub/theo-reijmers/42/811/260" target="_blank">Theo Reijmers</a> (3),
+<a href="http://www.linkedin.com/pub/robert-hall/2/792/88" target="_blank">Robert D. Hall</a> (1,2,3) and
+<a href="http://www.linkedin.com/pub/ric-de-vos/17/601/309" taget="_blank">Ric C.H. de Vos</a> (1,2,3)<br /><br />
 <i><b>1</b>. <a href="http://www.wageningenur.nl/nl/Expertises-Dienstverlening/Onderzoeksinstituten/Plant-Research-International.htm" target="_blank">Plant Research International, Wageningen-UR, Wageningen, The Netherlands</a>; <b>2</b>. <a href="http://www.cbsg.nl/" target="_blank">Centre for BioSystems Genomics, Wageningen, The Netherlands</a>; <b>3</b>. <a href="http://www.metabolomicscentre.nl/" target="_blank">Netherlands Metabolomics Centre, Leiden, The Netherlands</a></i>
             </div>
         </div>
